ac9cfeffe535851ddd08a3d3184a8736c34ed60b
nithyanandham-thiru-pydev <79956461+nithyanandham-thiru-pydev@users.noreply.github.com>
2021-03-15 10:24:06 -0700

    Add files via upload

---
+++ Withou_using_number_from 1to100.py
@@ -0,0 +1,19 @@
+# print number from 1 to 100  without using 1 to 100 any numbers in code  
+
+no_range = ord("a")
+
+# print(no_range)
+
+
+one = int(no_range/no_range)
+
+
+# print(one)
+
+
+name = ord("e")
+
+print(one,name)
+
+for x in range(one,name):
+	print(x)
